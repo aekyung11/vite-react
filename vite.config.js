36cc05c18d59b25f99f0f14dfa89fe66fcbb8ce3
aekyung11 <isaiah6014915@gmail.com>
2024-07-31 22:07:49 +0900

    feat: 절대 경로 사용 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
+const viteConfig = defineConfig({
+  base: "/",
+  server: {
+    host: "localhost",
+    port: 1117,
+    open: false,
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
+
+export default viteConfig;
